6c82831d0f38c6a3d35b1eefae9c333f1d538802
ellieene <ellieene@student.21-school.ru>
2025-03-01 15:43:36 +0700

    setting kafka, create bash start

--- build.sh
+++ build.sh
@@ -1,17 +1,57 @@
 #!/bin/bash
 
+# Запускаем Docker контейнеры
+echo "🚀 Запускаем Docker контейнеры..."
+docker-compose up --build db-chat -d
+docker-compose up --build db-news -d
+docker-compose up --build zookeeper -d
+docker-compose up --build kafka -d
+
+# Ждем, пока контейнеры запустятся
+echo "⏳ Ожидание готовности контейнеров..."
+sleep 10  # Даем время на запуск контейнеров
+
+# Проверяем, что контейнеры готовы
+for i in {1..10}; do
+    if docker exec postgres-news pg_isready; then
+        echo "✅ Контейнер postgres-news готов!"
+        break
+    fi
+    echo "❌ Ожидание готовности контейнера postgres-news..."
+    sleep 5
+done
+
+for i in {1..10}; do
+    if docker exec postgres-chat pg_isready; then
+        echo "✅ Контейнер postgres-chat готов!"
+        break
+    fi
+    echo "❌ Ожидание готовности контейнера postgres-chat..."
+    sleep 5
+done
+
+# Проверяем, что Kafka готов
+for i in {1..10}; do
+    if docker exec doctorai-kafka-1 /opt/kafka/bin/kafka-topics.sh --list --bootstrap-server localhost:9092; then
+        echo "✅ Контейнер Kafka готов!"
+        break
+    fi
+    echo "❌ Ожидание готовности контейнера Kafka..."
+    sleep 5
+done
+
 # Список папок с Maven-проектами
-PROJECTS=("DoctorAIChat" "DoctorAiNews" "Letter")
+PROJECTS=("DoctorAIChat" "DoctorAiNews" "Letter" "GigaChat")
 
 # Проходим по каждой папке и выполняем mvn clean install
 for PROJECT in "${PROJECTS[@]}"; do
     if [ -d "$PROJECT" ]; then
         echo "➡ Заходим в $PROJECT"
         cd "$PROJECT" || exit 1
-        
+    
         echo "🚀 Выполняем mvn clean package..."
         mvn clean package
-        
+    
         echo "✅ Готово: $PROJECT"
         cd ..
     else
@@ -19,5 +59,12 @@ for PROJECT in "${PROJECTS[@]}"; do
     fi
     echo "---------------------------"
 done
+
+# Сборка остальных контейнеров
+echo "🚀 Сборка остальных контейнеров..."
+docker-compose up --build chat -d
+docker-compose up --build news -d
+docker-compose up --build gigachat -d
+docker-compose up --build letter -d
 
 echo "🎉 Все сборки завершены!"
